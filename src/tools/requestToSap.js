53b2d7cd154ab09b31030a9e487cf312a29867d0
Fredy Castro <fredy.castro@clovit.com>
2024-07-12 11:27:59 -0500

    Messing with code

--- src/tools/requestToSap.js
+++ src/tools/requestToSap.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios" //messing with someone else's code
 
 const requestToSAP = (endpoint, body, path = 'send_external_system') => axios.post(`${process.env.REACT_APP_URL_API_MOVILIDAD}/${path}?url=` + encodeURIComponent(endpoint), body, {
     auth: {
