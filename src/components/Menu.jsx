42e0e011c36373b92e6b4a13d975d72236301181
higiniecito <cdramirezh@unal.edu.co>
2024-07-15 09:47:23 -0500

    use dropdown for menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./styles/Menu.scss";
 
 const MenuItem = ({ item }) => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -7,23 +10,23 @@ const MenuItem = ({ item }) => {
 	const hasSubItems = !!item.subItems;
 
 	return (
-		<>
+		<NavDropdown.Item as="div">
 			<div>
 				{!!hasSubItems && <span onClick={toggleMenu}>{isOpen ? "⬆️" : "🔽"}</span>}
 				<i className={item.icon} />
 				<Link to={item.target}>{item.title}</Link>
 			</div>
 			{!!hasSubItems && !!isOpen && <Menu menuItems={item.subItems} />}
-		</>
+		</NavDropdown.Item>
 	);
 };
 
 export const Menu = ({ menuItems }) => {
 	return (
-		<>
+		<NavDropdown>
 			{menuItems.map(item => {
 				return <MenuItem key={Math.random()} item={item} />;
 			})}
-		</>
+		</NavDropdown>
 	);
 };
