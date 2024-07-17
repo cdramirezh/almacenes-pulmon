793d4297dac5be1c617f82f0bf612b798293dcfe
higiniecito <cdramirezh@unal.edu.co>
2024-07-17 15:53:01 -0500

    Separate component so maybe I use it again later

--- src/NotificationsPage.jsx
+++ src/NotificationsPage.jsx
@@ -1,21 +1,23 @@
 import Toast from "react-bootstrap/Toast";
 import { notifications } from "./notifications.data";
 
+export const Notification = ({ notification }) => (
+	<Toast bg={notification.read ? "primary" : "light"}>
+		<Toast.Header closeButton={false}>
+			<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+			<strong className="me-auto">{notification.title}</strong>
+			<small>{notification.time}</small>
+		</Toast.Header>
+		<Toast.Body>{notification.body}</Toast.Body>
+	</Toast>
+);
+
 export const NotificationsPage = () => {
 	return (
 		<div className="notifications-page d-flex flex-column align-items-center">
-			{notifications.map((notification, idx) => {
-				return (
-					<Toast bg={notification.read ? "primary" : "light"} key={idx}>
-						<Toast.Header closeButton={false}>
-							<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-							<strong className="me-auto">{notification.title}</strong>
-							<small>{notification.time}</small>
-						</Toast.Header>
-						<Toast.Body>{notification.body}</Toast.Body>
-					</Toast>
-				);
-			})}
+			{notifications.map((notification, idx) => (
+				<Notification notification={notification} key={idx} />
+			))}
 		</div>
 	);
 };
