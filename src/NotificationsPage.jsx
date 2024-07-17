9feb7a418828c3b7c2c991b904dee4623bf1cca1
higiniecito <cdramirezh@unal.edu.co>
2024-07-17 14:36:01 -0500

    Notifications UI kind of ok

--- src/NotificationsPage.jsx
+++ src/NotificationsPage.jsx
@@ -1,3 +1,21 @@
+import Toast from "react-bootstrap/Toast";
+import { notifications } from "./notifications.data";
+
 export const NotificationsPage = () => {
-	return <div className="notifications-page">Notifica</div>;
+	return (
+		<div className="notifications-page d-flex flex-column align-items-center">
+			{notifications.map((notification, idx) => {
+				return (
+					<Toast bg={notification.read ? "primary" : "light"} key={idx}>
+						<Toast.Header closeButton={false}>
+							<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+							<strong className="me-auto">{notification.title}</strong>
+							<small>{notification.time}</small>
+						</Toast.Header>
+						<Toast.Body>{notification.body}</Toast.Body>
+					</Toast>
+				);
+			})}
+		</div>
+	);
 };
---
+++ src/notifications.data.js
@@ -0,0 +1,36 @@
+export const notifications = [
+	{
+		title: "Recordatorio de Mantenimiento",
+		body: "El mantenimiento programado para la cosechadora de caña de azúcar está próximo. Por favor, verifica el sistema hidráulico y reemplaza las piezas desgastadas.",
+		time: "Hace 11 min",
+		img: "https://example.com/maintenance-image.jpg",
+		link: "/detalles-mantenimiento/123",
+		read: false,
+	},
+	{
+		title: "Alerta de Emergencia",
+		body: "Se ha detectado un problema crítico en el sistema de cinta transportadora. Se requiere acción inmediata. Consulta el manual de emergencia para los pasos de solución de problemas.",
+		time: "Ayer",
+		img: "https://example.com/alert-image.jpg",
+		link: "/detalles-emergencia/456",
+		read: true,
+	},
+	{
+		title: "Nueva Actualización de Firmware",
+		body: "Hay una actualización de firmware disponible para la máquina de plantación de caña de azúcar. Descarga e instala la versión 2.1.0 para mejorar la eficiencia y reducir el tiempo de inactividad.",
+		time: "El sábado",
+		img: "https://example.com/firmware-image.jpg",
+		link: "/actualizacion-firmware",
+		read: false,
+	},
+	{
+		title: "Sesión de Capacitación",
+		body: "Únete a la sesión de capacitación sobre diagnóstico y técnicas de solución de problemas en la cosechadora de caña de azúcar. Fecha: 20 de julio de 2024, a las 2:00 PM en la sala de capacitación.",
+		time: "Hace dos semanas",
+		img: "https://example.com/training-image.jpg",
+		link: "/horario-capacitacion",
+		read: true,
+	},
+];
+
+// Puedes usar este array 'notifications' en tu aplicación.
