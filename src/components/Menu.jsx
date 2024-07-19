5d164a4c0f8b57bf402136cdaf6673de3774e96b
Fredy Castro <fredy.castro@clovit.com>
2024-07-19 14:01:25 -0500

    Retracting menu after touch

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Accordion from "react-bootstrap/Accordion";
 import Navbar from "react-bootstrap/Navbar";
@@ -5,36 +6,36 @@ import Offcanvas from "react-bootstrap/Offcanvas";
 import { menuData2 } from "../data/menu.data";
 import "./styles/Menu.scss";
 
-const MenuItem = ({ item }) => {
+const MenuItem = ({ item, setNavbarExpand }) => {
 	const hasSubItems = !!item.subItems;
 
 	return hasSubItems ? (
 		<>
 			<Accordion.Header>
 				{!!item.icon && <i className={item.icon} />}
-				<Link to={item.target}>{item.title}</Link>
+				<Link to={item.target} onClick={() => setNavbarExpand(false)}>{item.title}</Link>
 			</Accordion.Header>
 			{!!hasSubItems && (
 				<Accordion.Body>
-					<Menu menuItems={item.subItems} />
+					<Menu menuItems={item.subItems} setNavbarExpand={setNavbarExpand}/>
 				</Accordion.Body>
 			)}
 		</>
 	) : (
 		<div className="accordion-leaf">
 			{!!item.icon && <i className={item.icon} />}
-			<Link to={item.target}>{item.title}</Link>
+			<Link to={item.target} onClick={() => setNavbarExpand(false)}>{item.title}</Link>
 		</div>
 	);
 };
 
-export const Menu = ({ menuItems }) => {
+export const Menu = ({ menuItems, setNavbarExpand }) => {
 	return (
 		<Accordion defaultActiveKey="0">
 			{menuItems.map((item, i) => {
 				return (
 					<Accordion.Item eventKey={i} key={i}>
-						<MenuItem item={item}></MenuItem>
+						<MenuItem item={item} setNavbarExpand={setNavbarExpand}></MenuItem>
 					</Accordion.Item>
 				);
 			})}
@@ -43,20 +44,23 @@ export const Menu = ({ menuItems }) => {
 };
 
 export const OffcanvasMenu = () => {
+	const [navbarExpanded, setNavbarExpand] = useState(false);
 	return (
 		<>
 			<Navbar expand={false}>
-				<Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
+				<Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} onClick={() => setNavbarExpand(!navbarExpanded)}				/>
 				<Navbar.Offcanvas
 					id={`offcanvasNavbar-expand-false`}
 					aria-labelledby={`offcanvasNavbarLabel-expand-false`}
 					placement="end"
+					show={navbarExpanded}
+					onHide={() => setNavbarExpand(false)}
 				>
 					<Offcanvas.Header closeButton>
 						<Offcanvas.Title id={`offcanvasNavbarLabel-expand-false`}>Menu</Offcanvas.Title>
 					</Offcanvas.Header>
 					<Offcanvas.Body className="menu-container">
-						<Menu menuItems={menuData2} className="menu-con-mas-especificidad" />
+						<Menu menuItems={menuData2} setNavbarExpand={setNavbarExpand} className="menu-con-mas-especificidad" />
 					</Offcanvas.Body>
 				</Navbar.Offcanvas>
 			</Navbar>
