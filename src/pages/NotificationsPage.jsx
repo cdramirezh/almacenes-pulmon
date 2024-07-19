224cd07e8bd0430bbb8547118e799d2449b4208a
higiniecito <cdramirezh@unal.edu.co>
2024-07-19 13:43:09 -0500

    try to add link to notifications, but does not work

--- src/pages/NotificationsPage.jsx
+++ src/pages/NotificationsPage.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import Toast from "react-bootstrap/Toast";
 import { notifications } from "../notifications.data";
 import "./styles/NotificationsPage.scss";
 
 export const Notification = ({ notification }) => (
-	<Toast bg={notification.read ? "primary" : "light"}>
-		<Toast.Header closeButton={false}>
-			<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
-			<strong className="me-auto">{notification.title}</strong>
-			<small>{notification.time}</small>
-		</Toast.Header>
-		<Toast.Body>{notification.body}</Toast.Body>
-	</Toast>
+	<Link to={notification.link}>
+		<Toast bg={notification.read ? "primary" : "light"}>
+			<Toast.Header closeButton={false}>
+				<img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+				<strong className="me-auto">{notification.title}</strong>
+				<small>{notification.time}</small>
+			</Toast.Header>
+			<Toast.Body>{notification.body}</Toast.Body>
+		</Toast>
+	</Link>
 );
 
 export const NotificationsPage = () => {
