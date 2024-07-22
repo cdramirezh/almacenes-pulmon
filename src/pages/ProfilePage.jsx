ed10cb4acd151991172d49e27b2c2d03468bd97f
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-07-22 11:25:30 -0500

    Implementing fixing on profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,10 +2,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import { Breadcrumb, Image } from "react-bootstrap";
+import { Breadcrumb, Figure, Image } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 import "./styles/ProfilePage.scss";
-import { LinkContainer } from "react-router-bootstrap";
 
 const ProfilePage = () => {
 	const supplierData = {
@@ -43,33 +43,39 @@ const ProfilePage = () => {
 				<>
 					<Row>
 						<Col>
-							<Image src="https://t3.ftcdn.net/jpg/02/33/46/24/360_F_233462402_Fx1yke4ng4GA8TJikJZoiATrkncvW6Ib.jpg" />
-							<Form.Group>
+							<Figure className="image-container">
+								<Image className="image-profile" src="/images/profile.png" />
+							</Figure>
+							<Form.Group className="form-group">
 								<Row>
 									<Col md={6}>
-										<FloatingLabel label="Tipo de identificación">
-											<Form.Select name="idType" defaultValue={supplierData.DOC_TYPE} disabled>
-												<option value="">Seleccione un tipo de identificación</option>
-												<option value="13">Cédula de ciudadanía</option>
-												<option value="22">Cédula de extranjería</option>
-												<option value="31">NIT</option>
-												<option value="41">Pasaporte</option>
-											</Form.Select>
-											<div className="invalid-feedback">Indique tipo de identificación</div>
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Tipo de identificación">
+												<Form.Select name="idType" defaultValue={supplierData.DOC_TYPE} disabled>
+													<option value="">Seleccione un tipo de identificación</option>
+													<option value="13">Cédula de ciudadanía</option>
+													<option value="22">Cédula de extranjería</option>
+													<option value="31">NIT</option>
+													<option value="41">Pasaporte</option>
+												</Form.Select>
+												<div className="invalid-feedback">Indique tipo de identificación</div>
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 									<Col md={6}>
-										<FloatingLabel label="Número de identificación">
-											<Form.Control
-												name="idNumber"
-												defaultValue={supplierData.DOC_NUMBER}
-												disabled
-											/>
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Número de identificación">
+												<Form.Control
+													name="idNumber"
+													defaultValue={supplierData.DOC_NUMBER}
+													disabled
+												/>
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 								</Row>
 							</Form.Group>
-							<Form.Group>
+							<Form.Group className="form-group">
 								<FloatingLabel label="Razón Social">
 									<Form.Control
 										name="businessName"
@@ -78,7 +84,7 @@ const ProfilePage = () => {
 									/>
 								</FloatingLabel>
 							</Form.Group>
-							<Form.Group>
+							<Form.Group className="form-group">
 								<FloatingLabel label="Correo electrónico">
 									<Form.Control name="email" defaultValue={supplierData.EMAIL} disabled />
 								</FloatingLabel>
@@ -86,11 +92,13 @@ const ProfilePage = () => {
 							<Form.Group>
 								<Row>
 									<Col md={6}>
-										<FloatingLabel label="Celular">
-											<Form.Control name="mobile" defaultValue={supplierData.MOBILE} disabled />
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Celular">
+												<Form.Control name="mobile" defaultValue={supplierData.MOBILE} disabled />
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
-									<Col md={6}>
+									<Col md={6} className="form-group">
 										<FloatingLabel label="Teléfono">
 											<Form.Control
 												name="telephone"
@@ -104,49 +112,57 @@ const ProfilePage = () => {
 							<Form.Group>
 								<Row>
 									<Col md={6}>
-										<FloatingLabel label="País">
-											<Form.Control
-												name="country"
-												defaultValue={supplierData.COUNTRY_DESC}
-												disabled
-											/>
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="País">
+												<Form.Control
+													name="country"
+													defaultValue={supplierData.COUNTRY_DESC}
+													disabled
+												/>
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 									<Col md={6}>
-										<FloatingLabel label="Departamento">
-											<Form.Control
-												name="region"
-												defaultValue={supplierData.REGION_DESC}
-												disabled
-											/>
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Departamento">
+												<Form.Control
+													name="region"
+													defaultValue={supplierData.REGION_DESC}
+													disabled
+												/>
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 								</Row>
 							</Form.Group>
 							<Form.Group>
 								<Row>
 									<Col md={6}>
-										<FloatingLabel label="Ciudad">
-											<Form.Control name="city" defaultValue={supplierData.CITY_DESC} disabled />
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Ciudad">
+												<Form.Control name="city" defaultValue={supplierData.CITY_DESC} disabled />
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 									<Col md={6}>
-										<FloatingLabel label="Código postal">
-											<Form.Control
-												name="postalCode"
-												defaultValue={supplierData.POSTAL_CODE}
-												disabled
-											/>
-										</FloatingLabel>
+										<Form.Group className="form-group">
+											<FloatingLabel label="Código postal">
+												<Form.Control
+													name="postalCode"
+													defaultValue={supplierData.POSTAL_CODE}
+													disabled
+												/>
+											</FloatingLabel>
+										</Form.Group>
 									</Col>
 								</Row>
 							</Form.Group>
-							<Form.Group>
+							<Form.Group className="form-group">
 								<FloatingLabel label="Dirección">
 									<Form.Control name="address" defaultValue={supplierData.ADDRESS} disabled />
 								</FloatingLabel>
 							</Form.Group>
-							{/* <Form.Group>
+							{/* <Form.Group className="form-group">
 								<Row>
 									<Col md={6}>
 										<FloatingLabel label="Clase de impuesto">
@@ -168,7 +184,7 @@ const ProfilePage = () => {
 									</Col>
 								</Row>
 							</Form.Group>
-							<Form.Group>
+							<Form.Group className="form-group">
 								<FloatingLabel label="Condición de pago">
 									<Form.Control
 										name="paymentCondition"
