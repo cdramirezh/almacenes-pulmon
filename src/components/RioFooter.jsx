57ed90a1365cb8d11ec134e535905ee473a9c3c5
higiniecito <cdramirezh@unal.edu.co>
2024-07-22 11:55:55 -0500

    Add clovit logo to footer

--- src/components/RioFooter.jsx
+++ src/components/RioFooter.jsx
@@ -1,3 +1,10 @@
 import "./styles/RioFooter.scss";
 
-export const RioFooter = () => <footer>Desarrollado por Clovit</footer>;
+export const RioFooter = () => (
+	<footer className="footer-rio">
+		<img src="/clovit-Evolve-Consulting.png" alt="clovit" />
+		<a href="https://clovit.com/" target="_blank" rel="noreferrer">
+			<small>Desarrollado por Clovit ©</small>
+		</a>
+	</footer>
+);
