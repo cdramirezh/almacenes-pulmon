89eae146c6b260b06f6bf43a7012cdc5721ff7e0
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2024-07-23 16:48:57 -0500

    Removing link to Decor on HabeasDataNotification component

--- src/components/HabeasDataNotification.jsx
+++ src/components/HabeasDataNotification.jsx
@@ -11,7 +11,7 @@ const HabeasDataNotification = () => {
         <div className="habeas-data">
             {showNotification && 
                 <div className="wrapper">
-                    <p>Nuestro sitio web emplea cookies con el fin de brindarle un mejor servicio y garantizar una grata experiencia. Al seguir navegando, está aceptando la política de cookies. <a href="https://www.decorceramica.com/terminos-y-condiciones" target="_blank" rel="noreferrer">Ver más</a></p>
+                    <p>Nuestro sitio web emplea cookies con el fin de brindarle un mejor servicio y garantizar una grata experiencia. Al seguir navegando, está aceptando la política de cookies. <a href="#" target="_blank" rel="noreferrer">Ver más</a></p>
                     <div className="action">
                         <MyButton onClick={() => { setShowNotification(false); localStorage.setItem('habeasData', 'checked') }}>Cerrar</MyButton>
                     </div>
