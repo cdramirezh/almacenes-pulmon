73369e81c91261259e97ea4513854ebd106b7f5f
higiniecito <cdramirezh@unal.edu.co>
2024-07-23 15:01:17 -0500

    Change clovit logo

--- src/components/RioFooter.jsx
+++ src/components/RioFooter.jsx
@@ -2,7 +2,7 @@ import "./styles/RioFooter.scss";
 
 export const RioFooter = () => (
 	<footer className="footer-rio">
-		<img src="/clovit-Evolve-Consulting.png" alt="clovit" />
+		<img src="/Clovit.png" alt="clovit" />
 		<a href="https://clovit.com/" target="_blank" rel="noreferrer">
 			<small>Desarrollado por Clovit ©</small>
 		</a>
